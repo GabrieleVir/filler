32564703d077c0125e51f38bfcc4e42b6d406433
Gabriele Virga <Gabriele_Virga@hotmail.com>
2019-06-22 19:57:39 +0200

    New start

--- includes/filler.h
+++ includes/filler.h
@@ -1,100 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   filler.h                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gvirga <gvirga@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/15 02:42:45 by gvirga            #+#    #+#             */
-/*   Updated: 2019/06/07 21:34:45 by gvirga           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FILLER_H
 # define FILLER_H
 
-# include "libft.h"
-// For O_WRONLY (to remove after the project is finised)
-# include <fcntl.h>
+#include "libft.h"
 
-typedef struct			s_parser
+typedef struct      s_sog
 {
-	int					fd;
-	char				*line;
-	int					first_time;
-	int					i;
-	int					ret;
-}						t_parser;
+    char            **map;
+    char            player_char[2];
+    int             width;
+    int             height;
+    int             enemy_x;
+    int             enemy_y;
+    int             my_x;
+    int             my_y;
+    int             init_x;
+    int             init_y;
+    int             len_line;
+    char            *ret;
+}                   t_sog;
 
-typedef struct			s_filler_info
+typedef struct      s_piece
 {
-	char				*sogm;
-	char				*csogm;
-	char				*sogp;
-	char				*csogp;
-	int					player_number;
-	int					nb_rows;
-	int					nb_cols;
-	int					piece_rows;
-	int					piece_cols;
-	int					p_width;
-	int					p_height;
-	int					pos;
-	int					return_values[2];
-	char				player_char;
-}						t_filler_info;
-
-typedef struct			s_place
-{
-	int					i;
-	int					pos;
-	int					posf;
-	int					contact;
-	int					result;
-}						t_place;
-
-
-/*
-** listener.c
-*/
-
-int						vm_listener(t_parser **parser, t_filler_info **info);
-
-
-/*
-** strerror_manager.c
-*/
-int						error_msg_fd(int error, int fd);
-int						error_str_fd(char *str, int fd);
-int						error_info_malloc(int err_nb, t_parser **parser,
-											char *err_str);
-int						error_free_line(t_parser **parser);
-/*
-** info_player.c
-*/
-int						info_player(t_parser **parser, t_filler_info **info);
-int						init_variables(t_parser **p, t_filler_info **i);
-/*
-** read_sogp.c
-*/
-int						read_sogm(t_parser **parser, t_filler_info **info);
-/*
-** get_piece.c
-*/
-int						get_piece(t_parser **parser, t_filler_info **info);
-/*
-** piece_size.c
-*/
-void					get_real_width_height_piece(t_filler_info **info);
-
-/*
-** fill_map.c
-*/
-int						fill_map(t_filler_info **info, t_parser **parser);
-
-/*
-** fill_lbl.c
-*/
-int						fill_lbl(t_filler_info **info, t_parser **parser);
-
-#endif
+    int             tmp_x;
+    int             tmp_y;
+    int             contacts;
+    int             final_x;
+    int             final_y;
+    char            **piece;
+    int             width;
+    int             height;
+    int             real_width;
+    int             real_height;
+    int             init_x;
+    int             end_x;
+    int             init_y;
+    int             end_y;
+}                  t_piece;
